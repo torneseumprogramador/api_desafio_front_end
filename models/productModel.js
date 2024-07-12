52304eac5916555852a0a4e687d99cd8f84edd8d
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-07-12 07:45:51 -0300

    correção API

--- models/productModel.js
+++ models/productModel.js
@@ -1,10 +1,13 @@
-class Admin {
-    constructor(id, nome, email, senha) {
+class Product {
+    constructor(id, nome, preco, descricao, data_criacao, data_modificacao, quantidade_estoque) {
         this.id = id;
         this.nome = nome;
-        this.email = email;
-        this.senha = senha;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.data_criacao = data_criacao;
+        this.data_modificacao = data_modificacao;
+        this.quantidade_estoque = quantidade_estoque;
     }
 }
 
-module.exports = Admin;
+module.exports = Product;
