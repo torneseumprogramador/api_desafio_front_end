5de80cf2ceda2837bbf6744345533706c21dca95
Danilo Santos <danilo.aparecido.santos@gmail.com>
2024-07-14 06:03:47 -0300

    melhoria na doc home api

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.home = (req, res) => {
     res.json({
         message: 'Home da API criada no desafio',
-        documentation: `http://localhost:${process.env.PORT || 3000}/api-docs`
+        documentation: `/api-docs`
     });
 };
